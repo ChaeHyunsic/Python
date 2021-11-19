4fe540d8bc4070e49659e4c510f1bf69d33340fa
ChaeHyunsic <chaehyunsic630@gmail.com>
2021-11-19 12:03:55 +0900

    add fire, collision function

--- pygame_bubble/pygame_bubble.py
+++ pygame_bubble/pygame_bubble.py
@@ -1,5 +1,7 @@
 import pygame
 import os
+import random
+import math
 
 class bubble(pygame.sprite.Sprite):
     def __init__(self, image, color, position):
@@ -7,6 +9,27 @@ class bubble(pygame.sprite.Sprite):
         self.image = image
         self.color = color
         self.rect = image.get_rect(center=position)
+        self.radius = 18
+
+    def set_rect(self, position):
+        self.rect = self.image.get_rect(center = position)
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
+
+    def set_angle(self, angle):
+        self.angle = angle
+        self.rad_angle = math.radians(self.angle)
+
+    def move(self):
+        x_pos = self.radius * math.cos(self.rad_angle)
+        y_pos = self.radius * math.sin(self.rad_angle) * -1
+
+        self.rect.x += x_pos
+        self.rect.y += y_pos
+
+        if self.rect.left < 0 or self.rect.right > screen_width:
+            self.set_angle(180 - self.angle)
 
 class pointer(pygame.sprite.Sprite):
     def __init__(self, image, position, angle):
@@ -79,6 +102,61 @@ def get_image(color):
     else:
         return bubble_images[-1]
 
+def prepare_bubbles():
+    global current_bubble
+    global next_bubble
+
+    if next_bubble:
+        current_bubble = next_bubble
+    else:
+        current_bubble = create_bubble()
+
+    current_bubble.set_rect((screen_width //2, 624))
+    next_bubble = create_bubble()
+    next_bubble.set_rect((screen_width // 4, 688))
+
+def create_bubble():
+    color = get_color()
+    image = get_image(color)
+    return bubble(image, color, (0, 0))
+
+def get_color():
+    colors = []
+    for row in map:
+        for col in row:
+            if col not in colors and col not in [".", "/"]:
+                colors.append(col)
+    
+    return random.choice(colors)
+
+def collision():
+    global current_bubble, fire
+
+    col_bubble = pygame.sprite.spritecollideany(current_bubble, bubble_group, pygame.sprite.collide_mask)
+    if col_bubble or current_bubble.rect.top <= 0:
+        row_index, col_index = get_index(*current_bubble.rect.center)
+        place_bubble(current_bubble, row_index, col_index)
+        current_bubble = None
+        fire = False
+
+def get_index(x, y):
+    row_index = y // cell_size
+    col_index = x // cell_size
+
+    if row_index % 2 == 1:
+        col_index = (x - cell_size // 2) // cell_size
+        if col_index < 0:
+            col_index = 0
+        elif col_index > 6:
+            col_index = 6
+
+    return row_index, col_index
+
+def place_bubble(current_bubble, row_index, col_index):
+    map[row_index][col_index] = current_bubble.color
+    position = get_position(row_index, col_index)
+    current_bubble.set_rect(position)
+    bubble_group.add(current_bubble)
 
 pygame.init()   # pygame 시작
 
@@ -122,6 +200,9 @@ pointer_group = pointer(pointer_image, (screen_width // 2, 624), 90)
 angle_left = 0
 angle_right = 0
 angle_speed = 1.5
+current_bubble = None
+next_bubble = None
+fire = False    # 포인터에서 버블이 연사되지 않도록 하기 위해 사용
 
 setup()
 # 게임 진행 루프
@@ -138,6 +219,10 @@ while running:
                 angle_left += angle_speed
             elif event.key == pygame.K_RIGHT:
                 angle_right -= angle_speed
+            elif event.key == pygame.K_SPACE:
+                if current_bubble and not fire:
+                    fire = True
+                    current_bubble.set_angle(pointer_group.angle)
 
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT:
@@ -145,10 +230,24 @@ while running:
             elif event.key == pygame.K_RIGHT:
                 angle_right = 0
 
+    if not current_bubble:
+        prepare_bubbles()
+
+    if fire:
+        collision()
+
     screen.blit(background, (0,0))  # 게임 배경 지정
     bubble_group.draw(screen)
     pointer_group.rotate(angle_left + angle_right)
     pointer_group.draw(screen)
+
+    if current_bubble:
+        if fire:
+            current_bubble.move()
+        current_bubble.draw(screen)
+
+    if next_bubble:
+        next_bubble.draw(screen)
 
     pygame.display.update() # 변동사항 반영
 
